d3f75f856f0f6dfb867058989b4fa6e0f6fa34b8
Aniket Pant <aniketpant@jefcoed.com>
2020-03-05 09:33:51 -0600

    Updates

---
+++ precache-manifest.ae0e8729a8c2016636f871a20275ae74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d370f8243ca75e4c8974eed225fabc",
+    "url": "/orwell-map/index.html"
+  },
+  {
+    "revision": "bc5cfe629972711bb54b",
+    "url": "/orwell-map/static/css/main.6efa6943.chunk.css"
+  },
+  {
+    "revision": "f66808ef6a3b37184f81",
+    "url": "/orwell-map/static/js/2.195eae62.chunk.js"
+  },
+  {
+    "revision": "278573b4c188b9052aff83eaf7bb2ca3",
+    "url": "/orwell-map/static/js/2.195eae62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5cfe629972711bb54b",
+    "url": "/orwell-map/static/js/main.5f3c05a8.chunk.js"
+  },
+  {
+    "revision": "6832e8190dd21ddaf050",
+    "url": "/orwell-map/static/js/runtime-main.164995b6.js"
+  }
+]);
